60eeba18e8105312dd87b62bc3a8d358e68aef24
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2017-10-24 10:15:47 +0000

    Update register_config to register Media and Action too

--- tools/register_st2_config_to_zabbix.py
+++ tools/register_st2_config_to_zabbix.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 import sys
 
 from optparse import OptionParser
@@ -23,6 +24,8 @@ def get_options():
                       help="Login username to login Zabbix Server")
     parser.add_option('-p', '--password', dest="z_passwd", default='zabbix',
                       help="Password which is associated with the username")
+    parser.add_option('-s', '--sendto', dest="z_sendto", default='Admin',
+                      help="Address, user name or other identifier of the recipient")
 
     (options, args) = parser.parse_args()
 
@@ -30,6 +33,7 @@ def get_options():
         parser.error('Zabbix Server URL is not given')
 
     return (options, args)
+
 
 def is_already_registered(client, options):
     """
@@ -40,6 +44,7 @@ def is_already_registered(client, options):
             return True
 
     return False
+
 
 def register_media_type(client, options):
     """
@@ -55,12 +60,68 @@ def register_media_type(client, options):
     ]
 
     # send request to register a new MediaType for StackStorm
-    client.mediatype.create({
+    ret = client.mediatype.create(**{
         'description': 'StackStorm',
         'type': SCRIPT_MEDIA_TYPE,
         'exec_path': ST2_DISPATCHER_SCRIPT,
         'exec_params': "\n".join(mediatype_args) + "\n",
     })
+
+    return ret['mediatypeids'][0]
+
+
+def register_action(client, mediatype_id, options):
+    return client.action.create(**{
+        'name': 'Dispatching to StackStorm',
+        'esc_period': 360,
+        'eventsource': 0,  # means event created by a trigger
+        'def_shortdata': '{TRIGGER.STATUS}: {TRIGGER.NAME}',
+        'def_longdata': json.dumps({
+            'event': {
+                'id': '{EVENT.ID}',
+                'time': '{EVENT.TIME}',
+            },
+            'trigger': {
+                'id': '{TRIGGER.ID}',
+                'name': '{TRIGGER.NAME}',
+                'status': '{TRIGGER.STATUS}',
+            },
+            'items': [{
+                'name': '{ITEM.NAME%s}' % index,
+                'host': '{HOST.NAME%s}' % index,
+                'key': '{ITEM.KEY%s}' % index,
+                'value': '{ITEM.VALUE%s}' % index
+            } for index in range(1, 9)],
+        }),
+        'operations': [{
+            "operationtype": 0,
+            "esc_period": 0,
+            "esc_step_from": 1,
+            "esc_step_to": 1,
+            "evaltype": 0,
+            "opmessage_usr": [{"userid": "1"}],
+            "opmessage": {
+                "default_msg": 1,
+                "mediatypeid": mediatype_id,
+            }
+        }]
+    })
+
+
+def register_media_to_admin(client, mediatype_id, options):
+    return client.user.addmedia(**{
+        "users": [
+            {"userid": "1"},
+        ],
+        "medias": {
+            "mediatypeid": mediatype_id,
+            "sendto": options.z_sendto,
+            "active": "0",
+            "severity": "63",
+            "period": "1-7,00:00-24:00",
+        }
+    })
+
 
 def main():
     (options, _) = get_options()
@@ -70,21 +131,24 @@ def main():
                            user=options.z_userid,
                            password=options.z_passwd)
     except URLError as e:
-        print('Failed to connect Zabbix server (%s)' % e)
-        return 1
+        sys.exit('Failed to connect Zabbix server (%s)' % e)
     except ZabbixAPIException as e:
-        print('Failed to authenticate Zabbix (%s)' % e)
-        return 1
+        sys.exit('Failed to authenticate Zabbix (%s)' % e)
 
-    if not is_already_registered(client, options):
-        register_media_type(client, options)
-        print('New MediaType for StackStorm is registered successfully.')
-        return 0
-    else:
-        print('A MediaType for StackStorm has been already registered.')
-        return 1
+    if is_already_registered(client, options):
+        sys.exit('A MediaType for StackStorm has been already registered.')
+
+    # register a new MediaType to dispatch events to the StackStorm
+    mediatype_id = register_media_type(client, options)
+
+    # register a Action which is associated with the registered MediaType
+    register_action(client, mediatype_id, options)
+
+    # register a Media to the Admin user
+    register_media_to_admin(client, mediatype_id, options)
+
+    print('Success to register the configurations for StackStorm to the Zabbix Server.')
+
 
 if __name__ == '__main__':
-    exit_code = main()
-
-    sys.exit(exit_code)
+    main()
